d0feaf1f0f412565115c77d26f54983e88210d4b
PoonamHardikPatel <105453380+PoonamHardikPatel@users.noreply.github.com>
2022-09-16 18:11:26 -0400

    Commit - Final Assignment
    Commit - Final Assignment

--- script.js
+++ script.js
@@ -8,25 +8,34 @@
 $(document).ready(function(){
 
 	/* Cahnge Event is used... */
-	/*
-	$("input").change(function(){
+	$("input").change(function(){	// Selection of both input tag
+
+		// Declare a variables using css selectors - getting values from user with .val() method
 		let salary = $("#yearly-salary").val();
 		let percent = $("#percent").val();
 
+		// Mathematics - calculate amount to spent on tech 
 		let tech = ( ( salary * percent ) / 100 );
+
+		// Display the result
 		$("#amount").text('$' + tech.toFixed(2));
 	});
-	*/
 
 
 	/* Keyup Event is used... */
-	$("input").keyup(function(){
+	/*
+	$("input").keyup(function(){	// Selection of both input tag
+
+		// Declare a variables using css selectors - getting values from user with .val() method
 		let salary = $("#yearly-salary").val();
 		let percent = $("#percent").val();
 
+		// Mathematics - calculate amount to spent on tech 
 		let tech = ( ( salary * percent ) / 100 );
+
+		// Display the result
 		$("#amount").text('$' + tech.toFixed(2));
 	});
-
+	*/
 });
 
